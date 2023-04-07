c3c8d5e5690faa331a91eebe41d98cf68ae65a2c
ico0018 <siliwang.ico@gmail.com>
2023-04-07 22:58:34 +0800

    Update App.vue
    Basic recording and play.

--- chat-app/src/App.vue
+++ chat-app/src/App.vue
@@ -2,7 +2,9 @@
   <div id="app">
     <input v-model="inputMessage" placeholder="Type your message..." />
     <button @click="sendMessage" :disabled="isProcessing">Send</button>
-    <button @click="startRecording" :disabled="isProcessing">Start Recording</button>
+    <button @click="toggleRecording" :disabled="isProcessing">
+      {{ recording ? "Stop Recording" : "Start Recording" }}
+    </button>
     <div class="chat">
       <p v-for="message in messages" :key="message.id">{{ message.text }}</p>
     </div>
@@ -18,20 +20,12 @@ export default {
     return {
       inputMessage: "",
       messages: [],
+      recording: false,
       isProcessing: false,
+      recognition: null
     };
   },
   methods: {
-    async startRecording() {
-      this.isProcessing = true;
-      const recognition = new webkitSpeechRecognition();
-      recognition.lang = "en-US";
-      recognition.onresult = async (event) => {
-        this.inputMessage = event.results[0][0].transcript;
-        await this.sendMessage();
-      };
-      recognition.start();
-    },
     async sendMessage() {
       if (this.inputMessage.trim() !== "") {
         this.isProcessing = true;
@@ -47,8 +41,43 @@ export default {
         }
         this.inputMessage = "";
       }
+    },
+    toggleRecording() {
+      if (this.recording) {
+        this.stopRecording();
+      } else {
+        this.startRecording();
+      }
+    },
+    startRecording() {
+      if ("webkitSpeechRecognition" in window) {
+        this.recognition = new webkitSpeechRecognition();
+        this.recognition.lang = "en-US";
+        this.recognition.continuous = false;
+        this.recognition.interimResults = false;
+        this.recognition.onresult = (event) => {
+          this.inputMessage = event.results[0][0].transcript;
+          this.stopRecording();
+          this.sendMessage();
+        };
+        this.recognition.onerror = (event) => {
+          console.error("Error in speech recognition:", event.error);
+          this.stopRecording();
+        };
+        this.recognition.start();
+        this.recording = true;
+      } else {
+        alert("Speech recognition is not supported in this browser.");
+      }
+    },
+    stopRecording() {
+      if (this.recognition) {
+        this.recognition.stop();
+        this.recognition = null;
+      }
+      this.recording = false;
     }
-  },
+  }
 };
 </script>
 
